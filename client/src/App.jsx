a5fe20fcbe4b18fadaab43dc9cee02a5981cd3e3
Sid <sidoniomiguelsilva567@gmail.com>
2024-11-10 10:54:14 +0000

    Add Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,34 @@ import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.scss";
 import HomePage from "./pages/HomePage/HomePage";
+import Header from "./components/Header/Header";
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  const handleLogout = () => {
+    setUser(null); // Log out the user
+    alert("You have been logged out!");
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
+    <>
+      const [isDarkMode, setIsDarkMode] = useState(false);
+      <Header
+        user={user}
+        onLogout={handleLogout}
+        isDarkMode={isDarkMode}
+        toggleTheme={toggleTheme}
+      />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,47 @@
+const Header = ({ user, onLogout, isDarkMode, toggleTheme }) => {
+  return (
+    <header className="header">
+      <div className="header-container">
+        <h2>Packing List Generator</h2>
+        <div>
+          <div
+            className={`header__toggle-container ${
+              isDarkMode ? "dark" : "light"
+            }`}
+          >
+            <button className="header__toggle-button" onClick={toggleTheme}>
+              <span
+                className={`header__toggle-circle ${
+                  isDarkMode ? "dark" : "light"
+                }`}
+              ></span>
+            </button>
+            <span className="header__mode-text">
+              {isDarkMode ? "Dark Mode" : "Light Mode"}
+            </span>
+          </div>
+
+          {user ? (
+            <div className="header__user-info">
+              <span>Welcome, {user.name}</span>
+              <button onClick={onLogout}>Logout</button>
+            </div>
+          ) : (
+            <div className="header__auth-buttons">
+              <button onClick={() => alert("Login functionality coming soon!")}>
+                Login
+              </button>
+              <button
+                onClick={() => alert("Signup functionality coming soon!")}
+              >
+                Sign Up
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
