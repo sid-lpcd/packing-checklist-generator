2811392fc4b4e0b86fd721893956c78906d4c697
Sid <sidoniomiguelsilva567@gmail.com>
2024-11-11 16:11:03 +0000

    Full-stack deployment

--- server/index.js
+++ server/index.js
@@ -5,11 +5,28 @@ import listRoutes from "./routes/listRoutes.js";
 
 const app = express();
 
-// Only let certain URLs access this API - usually used for private APIs
-app.use(cors({ origin: "http://localhost:5173" }));
+const allowedOrigins =
+  process.env.NODE_ENV === "production"
+    ? ["https://your-production-url.com"] //to be updated
+    : ["http://localhost:5173"];
+app.use(cors({ origin: allowedOrigins }));
 
 app.use("/api/trip", tripRoutes);
 app.use("/api/list", listRoutes);
+
+// Serve static files from React in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(path.resolve(), "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(path.resolve(), "client/build", "index.html"));
+  });
+}
+
+// Error-handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Something went wrong!" });
+});
 
 const port = parseInt(process.env.PORT) || 3000;
 app.listen(port, () => {
